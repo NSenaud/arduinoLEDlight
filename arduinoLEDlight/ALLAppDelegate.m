f5487b0f65ad75112eb81a340aa919d0d10bd12f
Nicolas Senaud <nicolas@senaud.fr>
2014-05-24 10:35:45 +0200

    Initial Commit

---
+++ arduinoLEDlight/ALLAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  ALLAppDelegate.m
+//  arduinoLEDlight
+//
+//  Created by Nicolas Senaud on 2014/05/24.
+//  Copyright (c) 2014 Nicolas Senaud. All rights reserved.
+//
+
+#import "ALLAppDelegate.h"
+
+@implementation ALLAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
